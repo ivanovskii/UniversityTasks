a3eccd75b94db4e33f5a85382e2a31e4825f8bd3
Stas Ivanovskii <31916358+ivanovskii@users.noreply.github.com>
2018-08-15 14:01:20 +1000

    Create EratosthenesSieve.cpp

---
+++ AllTasks/SimpleTasks/EratosthenesSieve.cpp
@@ -0,0 +1,25 @@
+    #include <iostream>
+    #include <math.h>
+     
+    using namespace std;
+     
+    int main()
+    {
+        int n;
+        cin >> n;
+        cout << 2 << " " << 3 << " ";;
+        for (int i = 2; i<=n; i++)
+            for (int j = 2; j*j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    break;
+                }
+                else if (j + 1 > sqrt(i))
+                {
+                    cout << i << " ";
+                }
+            }
+        system("pause");
+        return 0;
+    }
